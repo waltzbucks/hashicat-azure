eba9bc8646ecb10d533c3e78bb529fd27c3ad232
Your Name <you@example.com>
2023-12-11 15:02:51 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -6,7 +6,9 @@ resource "azurerm_resource_group" "example" {
 }
 
 module "network" {
-  source              = "Azure/network/azurerm"
+  source  = "app.terraform.io/ACME-TF-WORKSHOP/network/azurerm"
+  version = "3.5.0"
+#   source              = "Azure/network/azurerm"
   resource_group_name = azurerm_resource_group.example.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
